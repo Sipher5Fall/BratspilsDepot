7e04010733ffd3c2b294852d5885e192f74067a9
dagmar94 <ida.dagmar@gmail.com>
2025-11-19 12:19:07 +0100

    Kurv

---
+++ Models/Kurv.cs
@@ -0,0 +1,18 @@
+﻿namespace BratspilsDepot.Models
+{
+    public class Kurv
+    {
+        private List<Spil> SpilIKurv = new List<Spil>();
+
+
+        public void TilføjSpil()
+        {
+            
+        }
+          
+        public void FjernSpil()
+        { 
+        
+        }
+    }
+}
